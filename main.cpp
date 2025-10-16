9a1598b43f751563ae8bb4aa56bcad8faaf87c2f
are you ladiesman 217? <damskiyugodnik981@gmail.com>
2025-10-16 13:41:49 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int choice;
+    cout << "Выберите время года:\n";
+    cout << "1. Лето\n";
+    cout << "2. Осень\n";
+    cout << "3. Зима\n";
+    cout << "4. Весна\n";
+    cout << "Ваш выбор: ";
+    cin >> choice;
+    if (choice = 1){
+        cout << "Наступило лето, наденьте пуховик!";
+}
+    else if (choice = 2) {
+        cout << "нееет, листьяя!";
+}
+    else if (choice = 3) {
+        cout << "С новым годом!";
+}
+    else {
+        cout << "Урааа, листьяяя";
+}
+}
